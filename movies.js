9fbb2d44785b71521acdbd878cc04044d4ad2bf9
alirsadiq <72763457+alirsadiq@users.noreply.github.com>
2021-03-09 22:09:40 -0700

    test

--- movies.js
+++ movies.js
@@ -1,52 +1,20 @@
+//Inspired by User.js file
+
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-var bcrypt = require('bcrypt-nodejs');
 
-mongoose.promise = global.Promise;
+mongoose.Promise = global.Promise;
 
-try {
-    mongoose.connect( process.env.DB, {useNewUrlParser: true, useUnifiedTopology: true}, () =>
-        console.log("connected"));
-}catch (error) {
-    console.log("could not connect");
-}
+mongoose.connect(process.env.DB, { useNewUrlParser: true } );
+mongoose.set('useCreateIndex', true);
 
-var movieSchema = new Schema ({
-    title: {type: String, required: true, index: {unique: true}},
-    year: {type: String, required:true},
-    genre: {
-        type:string,
-        required: true,
-        enum: ["Action, Adventure", "Comedy", "Drama", "Fantasy", "Horror", "Mystery", "Thriller", "Western"]
-    },
-    actors: [
-        {
-            actor_name: {type: String, required: true}, character_name: {type: String, required: true}
-        },
-        {
-            actor_name: {type: String, required: true}, character_name: {type: String, required: true}
-        },
-        {
-            actor_name: {type: String, required: true}, character_name: {type: String, required: true}
-        },
-
-    ]
-
+// movies schema
+var MovieSchema = new Schema({
+    title: { type: String, required: true, index: { unique: true }},
+    yearReleased: { type: Date, required: true },
+    genre: { type: String, required: true, enum: ['Action', 'Adventure', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Mystery', 'Thriller', 'Western'] },
+    actors: { type: [{actorName: String, characterName: String}], required: true }
 });
 
-movieSchema.pre("save", function (next) {
-    var movie = this
-
-    if (movie.actors.length < 3) {
-        console.log(" You didn't enter three actors")
-        var error = new ValidationError(this);
-        error.errors.movie = new ValidationError("Add more members");
-        next(error);
-    }
-    else
-    {
-        next();
-    }
-});
-
-module.exports = mongoose.model('Movies', movieSchema);
+// return the model
+module.exports = mongoose.model('Movie', MovieSchema);
